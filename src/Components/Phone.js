c8c93df3c083f82af609bcee2dd70cc1603ee918
Abhi06027 <abhinavk67@gmail.com>
2022-02-03 00:54:45 +0530

    netflix 2

---
+++ src/Components/Phone.js
@@ -0,0 +1,40 @@
+import React from 'react';
+// import tv from './tv.m4v'
+export default function Phone() {
+  return <div>
+ <h1 className=' text-white px-5 sm:px-5 md:px-8 lg:px-16  mt-2 sm:mt-2 md:mt-2  lg:mt-5  text-sm sm:text-sm md:text-sm  lg:text-2xl  font-bold  ' >
+             Top 10 in India Today  
+             
+                 </h1>
+                 
+             <div className=' flex  px-16 space-x-20 object-contain  py-2 sm:py-2 md:py-2 lg:py-3  overflow-scroll scrollbar-hide cursor-pointer     ' > 
+                 
+                 <img className=" text-white   w-[150px] h-[250px] object-contain   rounded-md transform hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/13.jpg" alt="" />
+               
+                   <img className=" text-white w-[150px] h-[250px]  object-contain    rounded-md transform hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/14.png" alt="" />
+                
+               
+                <img className=" text-white    w-[150px] h-[250px]    object-contain   rounded-md transform hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/15.png" alt="" />
+              
+                 
+                  
+                 <img className=" text-white   w-[150px] h-[250px]  object-contain  rounded-md transform hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/16.png" alt="" />
+                 
+                  
+                  
+                 
+           
+               <img className=" text-white   w-[150px] h-[250px]  object-contain   rounded-md transform hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/17.png" alt="" />
+               <img className=" text-white w-[150px] h-[250px]   object-contain  rounded-md transform hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/18.png" alt="" />
+               <img className=" text-white w-[150px] h-[250px]   object-contain  rounded-md transform hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/19.png" alt="" />
+            
+                <img className=" text-white w-[150px] h-[250px]   object-contain  rounded-md transform hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/20.png" alt="" />
+     <img className=" text-white w-[150px] h-[250px]   object-contain  rounded-md transform hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/21.png" alt="" />
+    </div>
+
+
+
+
+
+  </div>;
+}
